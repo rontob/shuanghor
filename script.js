27608238adce8fac25f45ced581e77845d9429f3
rontob <ronaldreyner@gmail.com>
2026-05-31 23:27:33 +0700

    Add support for YouTube and image media types

--- script.js
+++ script.js
@@ -81,3 +81,25 @@ function getYoutubeId(url){
     ? match[2]
     : '';
 }
+if(item.type === "youtube"){
+
+    media = `
+    <iframe
+      class="video"
+      src="https://www.youtube.com/embed/${ytId}"
+      allowfullscreen>
+    </iframe>
+    `;
+
+}
+
+if(item.type === "image"){
+
+    media = `
+    <img
+      src="${item.image}"
+      class="poster"
+      alt="${item.judul}">
+    `;
+
+}
